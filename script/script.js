2ce0afbd16b136fd43cab94c79cd9cc4df039ad8
Anna <vinnik.anna@rambler.ru>
2022-08-20 14:45:05 +0700

    Добавлено появление описания в card-compact

---
+++ script/script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const cardCompact = {
+    linksOnPage: document.querySelectorAll(".card__compact"),
+
+    toggleDescription() {
+        this.linksOnPage.forEach( (elem) => {
+            let children = elem.childNodes;
+            let img;
+            let description;
+    
+            for (let elem of children) {    
+                if (elem.classList?.contains("card-compact__img")){
+                    img = elem;
+                    continue;
+                }
+                if (elem.classList?.contains("card-compact__description")){
+                    description = elem;
+                }
+            }
+            
+            elem.addEventListener('mouseover', function() {
+                img.style.opacity = "0.3";
+                description.style.display = "block";
+            });
+            elem.addEventListener('mouseout', function() {
+                img.style.opacity = "1";
+                description.style.display = "none";
+            });
+        });
+    }
+}
+
+cardCompact.toggleDescription();
